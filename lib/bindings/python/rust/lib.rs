6c890315bee15d90e08271da8748cd2ccbfb60bc
Piotr Tarasiewicz <ptarasiewicz@nvidia.com>
2026-08-20 13:10:37 +0200

    feat(vllm): add audio transcription serving
    Signed-off-by: Piotr Tarasiewicz <ptarasiewicz@nvidia.com>

--- lib/bindings/python/rust/lib.rs
+++ lib/bindings/python/rust/lib.rs
@@ -1046,6 +1046,10 @@ impl ModelType {
         inner: llm_rs::model_type::ModelType::Audios,
     };
     #[classattr]
+    const Transcriptions: Self = ModelType {
+        inner: llm_rs::model_type::ModelType::Transcriptions,
+    };
+    #[classattr]
     const Videos: Self = ModelType {
         inner: llm_rs::model_type::ModelType::Videos,
     };
